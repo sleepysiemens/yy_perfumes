cd02bea711b89d9697eb8c1e6cc28aac0b21a559
GQuantic <darkkukri0550@gmail.com>
2023-10-19 00:10:22 +0300

    Merchant

--- app/Listeners/OrderCreated/YooKassaRedirect.php
+++ app/Listeners/OrderCreated/YooKassaRedirect.php
@@ -39,6 +39,7 @@ class YooKassaRedirect
         $client = $yookassaService->getClient();
         $idempotenceKey = uniqid($event->order->id, true);
 
+
         // Items
         $items = [];
         foreach ($event->order->basket['cart'] as $item) {
@@ -69,28 +70,28 @@ class YooKassaRedirect
                     'email' => $event->order->email,
                     'phone' => $event->order->phone,
                 ],
-//                'items' => [
-//                    [
-//                        'description' => 'Переносное зарядное устройство Хувей',
-//                        'quantity' => '1.00',
-//                        'amount' => [
-//                            'value' => 1000,
-//                            'currency' => 'RUB'
-//                        ],
-//                        'vat_code' => '2',
-//                        'payment_mode' => 'full_payment',
-//                        'payment_subject' => 'commodity',
-//                        'country_of_origin_code' => 'CN',
-//                        'product_code' => '44 4D 01 00 21 FA 41 00 23 05 41 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 12 00 AB 00',
-//                        'customs_declaration_number' => '10714040/140917/0090376',
-//                        'excise' => '20.00',
-//                        'supplier' => [
-//                            'name' => 'string',
-//                            'phone' => 'string',
-//                            'inn' => 'string'
-//                        ]
-//                    ],
-//                ]
+                'items' => [
+                    [
+                        'description' => 'Товары',
+                        'quantity' => '1.00',
+                        'amount' => [
+                            'value' => round($event->order->total),
+                            'currency' => 'RUB'
+                        ],
+                        'vat_code' => '2',
+                        'payment_mode' => 'full_payment',
+                        'payment_subject' => 'commodity',
+                        'country_of_origin_code' => 'CN',
+                        'product_code' => '44 4D 01 00 21 FA 41 00 23 05 41 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 12 00 AB 00',
+                        'customs_declaration_number' => '10714040/140917/0090376',
+                        'excise' => '20.00',
+                        'supplier' => [
+                            'name' => 'Сергей',
+                            'phone' => '+79956112267',
+                            'inn' => '052401862060'
+                        ]
+                    ],
+                ]
             ],
             $idempotenceKey
         ]);
