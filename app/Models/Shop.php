9c34cf90ca9b255b992c558967815e37b093362d
GQuantic <darkkukri0550@gmail.com>
2023-10-21 22:42:38 +0300

    Updates

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -9,6 +9,8 @@ class Shop extends Model
 {
     use HasFactory;
 
+    protected $guarded = [];
+
     protected $casts = [
         'phone' => 'string'
     ];
--- app/Models/User.php
+++ app/Models/User.php
@@ -44,7 +44,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-    
+
     protected $guarded = [];
 
     /**
